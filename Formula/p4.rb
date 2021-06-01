9975e8750e36db35bb49ee47e3857e848d0d0c1d
Ferdinand Prantl <prantlf@gmail.com>
2021-06-01 09:04:52 +0200

    Update badge support data

--- Formula/p4.rb
+++ Formula/p4.rb
@@ -1,15 +1,15 @@
 class P4 < Formula
   desc "Helix Command-Line Client (P4) provides administrators and heavyweight users complete control over the system, custom reports, automation, and facilitates cross-platform consistency."
   homepage "https://www.perforce.com/downloads/helix-command-line-client-p4"
-  version "2020.1.2107780"
+  version "2021.1.2126753"
   bottle :unneeded
 
   if OS.mac?
-    url "https://www.perforce.com/downloads/perforce/r20.1/bin.macosx1010x86_64/helix-core-server.tgz"
-    sha256 "e93b556b4824afa3b9c1b7e328a53c605c8953cc9facd6c12d7a3b3451434ea6"
+    url "https://cdist2.perforce.com/perforce/r21.1/bin.macosx1015x86_64/helix-core-server.tgz"
+    sha256 "9a1f528f92568531785b1f125e60a48e84b1a958f9e1d74dfc6e61c060e08721"
   elsif OS.linux? and Hardware::CPU.intel?
-    url "https://www.perforce.com/downloads/perforce/r20.1/bin.linux26x86_64/helix-core-server.tgz"
-    sha256 "cbe414da36ef7b0ad9bb913e92d37603594c3c4d4244c8791c81886420cfaf6d"
+    url "https://cdist2.perforce.com/perforce/r21.1/bin.linux26x86_64/helix-core-server.tgz"
+    sha256 "b78c37e44f67ebf49b92783a434290f3570edda25386ca55053d2a93456f8c18"
   end
 
   def install
