d3445ac19d915bb09742e61e1f4adba98ca21180
Ferdinand Prantl <prantlf@gmail.com>
2021-06-04 16:27:13 +0200

    Point to the new lite-xl website, rename the symlink to lite
    Do not mark the app as self-autoupdating.

--- Casks/lite-xl.rb
+++ Casks/lite-xl.rb
@@ -2,21 +2,22 @@ cask "lite-xl" do
   version "1.16.11"
   sha256 "b3d13a38c90d83a1af4b8c3ff28d29763f6d34f7018c7b2dd2dd07f5cb4fc7de"
 
-  url "https://github.com/franko/lite-xl/releases/download/v#{version}/lite-xl-macos-x86-64.dmg"
+  url "https://github.com/franko/lite-xl/releases/download/v#{version}/lite-xl-macos-x86-64.dmg",
+      verified: "github.com/franko/lite-xl/"
+  appcast "https://github.com/franko/lite-xl/releases.atom"
   name "Lite XL"
   desc "Lightweight text editor written in Lua"
-  homepage "https://github.com/franko/lite-xl#readme"
+  homepage "https://lite-xl.github.io/"
 
   livecheck do
     url :url
     strategy :github_latest
   end
 
-  auto_updates true
   depends_on macos: ">= :high_sierra"
 
   app "lite-xl.app"
-  binary "#{appdir}/lite-xl.app/Contents/MacOS/lite-xl"
+  binary "#{appdir}/lite-xl.app/Contents/MacOS/lite-xl", target: "lite"
 
   zap trash: ["~/.config/lite-xl"]
 end
