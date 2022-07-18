145478e93034306bed601c334c58eb567da31379
Ferdinand Prantl <prantlf@gmail.com>
2022-07-18 15:57:09 +0200

    Fix installation of HelixMFA.app and related

--- Casks/helix-mfa.rb
+++ Casks/helix-mfa.rb
@@ -7,9 +7,21 @@ cask "helix-mfa" do
   desc "Provides the MFA login screen for Helix Core clients and plugins - Perforce, for example"
   homepage "https://www.perforce.com/downloads/helix-mfa-authenticator"
 
-  depends_on macos: ">= :high_sierra"
-  container type: :naked
+  depends_on macos: ">= :el_capitan"
 
-  # app "HelixMFA.app"
-  # binary "#{appdir}/HelixMFA.app/Contents/MacOS/HelixMFA", target: "helixmfa"
+  livecheck do
+    url "https://www.perforce.com/support/software-release-index#tab-panel-6258"
+    regex(%r{(?:Patch|Release) for[^<]+?Helix MFA Authenticator[^<]+?v?(\d+(?:\.\d+)+)/(\d+)}im)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
+    end
+  end
+
+  app "HelixMFA.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.perforce.Helix",
+    "~/Library/Preferences/com.perforce.Helix.plist",
+    "~/Library/Saved Application State/com.perforce.HelixMFA.savedState"
+  ]
 end
