2c7f7d9af572e70b1084452eb85094d16d0954e0
Ferdinand Prantl <prantlf@gmail.com>
2023-12-17 22:41:50 +0100

    Update formulae

--- Formula/update-formula.rb
+++ Formula/update-formula.rb
@@ -1,25 +1,25 @@
 class UpdateFormula < Formula
   desc "Updates version numbers and SHA-256 hashes in Homebrew formula files for GitHub releases."
   homepage "https://github.com/prantlf/update-formula"
-  version "0.1.2"
+  version "0.1.3"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/prantlf/update-formula/releases/download/v0.1.2/update-formula-macos-x64.zip"
-      sha256 "222ecac93f733ecc93c9b526d2fc64314517e3c65bbb50125ef6feb55f16bc80"
+      url "https://github.com/prantlf/update-formula/releases/download/v0.1.3/update-formula-macos-x64.zip"
+      sha256 "588ba66e197c23ba8a57d7daa295d84a2c22380a6499a4bd576bb6971353782c"
     elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/prantlf/update-formula/releases/download/v0.1.2/update-formula-macos-arm64.zip"
-      sha256 "b3ceee56d11ca4f356313f8e5b5bbb5e4cda8b1c1c886043d8baf2d38ab7ecab"
+      url "https://github.com/prantlf/update-formula/releases/download/v0.1.3/update-formula-macos-arm64.zip"
+      sha256 "7e2eff55b29b6dbffb6ab7cde2e5a1a28b02368b33640f934816f5add15eafee"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/prantlf/update-formula/releases/download/v0.1.2/update-formula-linux-x64.zip"
-      sha256 "d50aef1724fd4bbf1ff82c14dcba77c99301ee1cf4fbaa2844434ce113f2604d"
+      url "https://github.com/prantlf/update-formula/releases/download/v0.1.3/update-formula-linux-x64.zip"
+      sha256 "1e5497df2ac057ad6e89d5eeec213cf6f61feaafccb7550e6d338de682527734"
     elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/prantlf/update-formula/releases/download/v0.1.2/update-formula-linux-arm64.zip"
-      sha256 "57d6bcae1f67e2b3f83b5b013197bd5d7644ed3fb11e359aefb021ed5224b6ef"
+      url "https://github.com/prantlf/update-formula/releases/download/v0.1.3/update-formula-linux-arm64.zip"
+      sha256 "23bc940e0a4e5e69d56ea767c48ed5e7c691abccfcd8f5ed790cd80ff5e780fe"
     end
   end
 
--- Formula/vp.rb
+++ Formula/vp.rb
@@ -1,25 +1,25 @@
 class Vp < Formula
   desc "Helps with development, installation and maintenance of VPM packages."
   homepage "https://github.com/prantlf/vp"
-  version "0.9.0"
+  version "0.10.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/prantlf/vp/releases/download/v0.9.0/vp-macos-x64.zip"
-      sha256 "c9383bd19b35c2bb9df8775796666ce2561b7decd1f8612239a4f60f84aaab6c"
+      url "https://github.com/prantlf/vp/releases/download/v0.10.0/vp-macos-x64.zip"
+      sha256 "e3ae86eb06e52b510c67845e91e6bd737f2cbda15077b7ced24801e94dca8c2d"
     elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/prantlf/vp/releases/download/v0.9.0/vp-macos-arm64.zip"
-      sha256 "a3051812c05ff6011521c8d82bf96e40136d37805c23728cb74e15c11bb3cda4"
+      url "https://github.com/prantlf/vp/releases/download/v0.10.0/vp-macos-arm64.zip"
+      sha256 "efadb71f38be172d9b0e848748c57e8fecd72c02c87ac4e61a77103e9186153c"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/prantlf/vp/releases/download/v0.9.0/vp-linux-x64.zip"
-      sha256 "1159419922d473e5fbf9d1a86a74c0e171427fcb39ff0a924a6866ca6345276b"
+      url "https://github.com/prantlf/vp/releases/download/v0.10.0/vp-linux-x64.zip"
+      sha256 "ce1adb13a3b5bb0122dc85d9ee07724b4f91a94cc4daf5b48fae8067fac7b258"
     elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/prantlf/vp/releases/download/v0.9.0/vp-linux-arm64.zip"
-      sha256 "b0d1c3879f1c249ae8882c0eb7e34abafd83b29dd07d4a783888a4097ba03a5f"
+      url "https://github.com/prantlf/vp/releases/download/v0.10.0/vp-linux-arm64.zip"
+      sha256 "77dbdb2d7bb49e9081c96bc9971d3a398c026a150ad572930503ac75c70812e5"
     end
   end
 
