a287de3db6a656b898fb82b60a217b510e236e63
Ferdinand Prantl <prantlf@gmail.com>
2024-04-01 20:09:52 +0200

    Update badge support data

--- Formula/vp.rb
+++ Formula/vp.rb
@@ -1,25 +1,25 @@
 class Vp < Formula
   desc "Helps with development, installation and maintenance of VPM packages."
   homepage "https://github.com/prantlf/vp"
-  version "0.13.0"
+  version "0.14.1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/prantlf/vp/releases/download/v0.13.0/vp-macos-x64.zip"
-      sha256 "40ab170f45c8f6d1fc1ae5790d2bb6b077afcb017edba3dcdf6d831d65b0607e"
+      url "https://github.com/prantlf/vp/releases/download/v0.14.1/vp-macos-x64.zip"
+      sha256 "a85b38f81f5a6cba2d945fb6d07a946059834ae88a6f2c2abb8e4e63abe66f76"
     elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/prantlf/vp/releases/download/v0.13.0/vp-macos-arm64.zip"
-      sha256 "3047c17d7bb765156b814b1b4801ce41db7c6c8e9bd4fab138f1a1b2fafa23d7"
+      url "https://github.com/prantlf/vp/releases/download/v0.14.1/vp-macos-arm64.zip"
+      sha256 "5a58f13a7b80bd3c032e202cc385c99f5a3d4c9e6f5ad80952f5b96be7c691ba"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/prantlf/vp/releases/download/v0.13.0/vp-linux-x64.zip"
-      sha256 "26b435e502a85a9da1eab20735690538fa62f7edc7d98b2225738dcac157ea76"
+      url "https://github.com/prantlf/vp/releases/download/v0.14.1/vp-linux-x64.zip"
+      sha256 "024ae9683deb4c74257f0083c9e2964479415787e68f9a18d558fb524ed4ed3f"
     elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/prantlf/vp/releases/download/v0.13.0/vp-linux-arm64.zip"
-      sha256 "09f8d5c454b26dd2f6c60a390fdf4c8b937f08df0ae453216a1a266faff8e600"
+      url "https://github.com/prantlf/vp/releases/download/v0.14.1/vp-linux-arm64.zip"
+      sha256 "96709c0ee1006d03b96bd3bcc7f637ea8903c215a667c828d9d10ddf8120658f"
     end
   end
 
