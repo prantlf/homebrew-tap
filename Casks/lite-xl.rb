9455d900ec98ca3b6de65e4948229301d15eddb7
Jonathan Hult <jonathan@jonathanhult.com>
2024-04-28 05:49:44 -0500

    Update Lite XL (#1)
    Update repository URL
    
    Co-authored-by: Ferdinand Prantl <prantlf@gmail.com>

--- Casks/lite-xl.rb
+++ Casks/lite-xl.rb
@@ -5,15 +5,16 @@ cask "lite-xl" do
   version "2.1.4"
 
   if Hardware::CPU.intel?
-    url "https://github.com/lite-xl/lite-xl/releases/download/v2.1.4/lite-xl-v#{version}-macos-x86_64.dmg",
-      verified: "github.com/franko/lite-xl/"
+    url "https://github.com/lite-xl/lite-xl/releases/download/v#{version}/lite-xl-v#{version}-macos-x86_64.dmg",
+      verified: "github.com/lite-xl/lite-xl/"
     sha256 "95d634e56fac7ceea408c79e5fd82f955360751a094864fe3a783478bef5615c"
   elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/lite-xl/lite-xl/releases/download/v2.1.4/lite-xl-v#{version}-macos-arm64.dmg",
-      verified: "github.com/franko/lite-xl/"
+    url "https://github.com/lite-xl/lite-xl/releases/download/v#{version}/lite-xl-v#{version}-macos-arm64.dmg",
+      verified: "github.com/lite-xl/lite-xl/"
     sha256 "5e26cfee4d3d02fbad34ec4507733ac749c12191d391669129754260fc1ae547"
   end
 
+  appcast "https://github.com/lite-xl/lite-xl/releases.atom"
   livecheck do
     url :url
     strategy :github_latest
@@ -23,7 +24,7 @@ cask "lite-xl" do
 
   app "Lite XL.app"
 
-  caveats "The symbolic link `lite` doesn't work any more. Create a shell alias instead.\nYou may just execute it, or include it in .hashrc or .zshrc:\n\nalias lite='\"/Applications/Lite XL.app/Contents/MacOS/lite-xl\"'"
+  caveats "The symbolic link `lite` doesn't work any more. Create a shell alias instead.\nYou may just execute the following command, or include it in .hashrc or .zshrc:\n\nalias lite='\"/Applications/Lite XL.app/Contents/MacOS/lite-xl\"'"
 
   zap trash: ["~/.config/lite-xl"]
 end
