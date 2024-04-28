720c39f85a84e35c27506844b1b985685bf3766e
Ferdinand Prantl <prantlf@gmail.com>
2024-04-28 11:41:41 +0200

    Add verified URL to lite-xl

--- Casks/lite-xl.rb
+++ Casks/lite-xl.rb
@@ -5,10 +5,12 @@ cask "lite-xl" do
   version "2.1.4"
 
   if Hardware::CPU.intel?
-    url "https://github.com/lite-xl/lite-xl/releases/download/v2.1.4/lite-xl-v#{version}-macos-x86_64.dmg"
+    url "https://github.com/lite-xl/lite-xl/releases/download/v2.1.4/lite-xl-v#{version}-macos-x86_64.dmg",
+      verified: "github.com/franko/lite-xl/"
     sha256 "95d634e56fac7ceea408c79e5fd82f955360751a094864fe3a783478bef5615c"
   elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/lite-xl/lite-xl/releases/download/v2.1.4/lite-xl-v#{version}-macos-arm64.dmg"
+    url "https://github.com/lite-xl/lite-xl/releases/download/v2.1.4/lite-xl-v#{version}-macos-arm64.dmg",
+      verified: "github.com/franko/lite-xl/"
     sha256 "5e26cfee4d3d02fbad34ec4507733ac749c12191d391669129754260fc1ae547"
   end
 
