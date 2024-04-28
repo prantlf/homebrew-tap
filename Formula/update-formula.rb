b9338afbe8f0da2c30146bfff006cb199f5a06b0
Ferdinand Prantl <prantlf@gmail.com>
2024-04-28 13:36:14 +0200

    Update update-formula

--- Formula/update-formula.rb
+++ Formula/update-formula.rb
@@ -1,25 +1,25 @@
 class UpdateFormula < Formula
   desc "Updates version numbers and SHA-256 hashes in Homebrew formula files for GitHub releases."
   homepage "https://github.com/prantlf/update-formula"
-  version "0.1.4"
+  version "0.2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/prantlf/update-formula/releases/download/v0.1.4/update-formula-macos-x64.zip"
-      sha256 "ae7c079dd4c1a8812257d32427961044a8b01fff685853809668f191dec669e2"
+      url "https://github.com/prantlf/update-formula/releases/download/v0.2.0/update-formula-macos-x64.zip"
+      sha256 "78f3e1f26a89ed6831db999e2249373e19663797e7a01ba93c34fb95f1e24702"
     elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/prantlf/update-formula/releases/download/v0.1.4/update-formula-macos-arm64.zip"
-      sha256 "2a90463e6e3755d435655cb922501adf679ee59b4d737369d655dbaf2c682464"
+      url "https://github.com/prantlf/update-formula/releases/download/v0.2.0/update-formula-macos-arm64.zip"
+      sha256 "e6175b2bf1d337046c0050d3dfc88b4a82bebd9445fcd8499be9457df5ffd665"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/prantlf/update-formula/releases/download/v0.1.4/update-formula-linux-x64.zip"
-      sha256 "08fc5dac0c6fb8fa5c66f69bd8ae84987ff1ba2b822726b37a2985d7e3c25b92"
+      url "https://github.com/prantlf/update-formula/releases/download/v0.2.0/update-formula-linux-x64.zip"
+      sha256 "788ae7c1d8eae3c4c6c03f48ac4f9ba3b5138c981079dc1216fc5e640aa8d71d"
     elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/prantlf/update-formula/releases/download/v0.1.4/update-formula-linux-arm64.zip"
-      sha256 "77ae805d3886c79fbf94a4fb337a1874bb249a957f2728d1375e836ce053c3bd"
+      url "https://github.com/prantlf/update-formula/releases/download/v0.2.0/update-formula-linux-arm64.zip"
+      sha256 "3a07414897bd36be031c3b2bcf018f592bf6f96dbd3bf57e9b145e1e685ec75b"
     end
   end
 
